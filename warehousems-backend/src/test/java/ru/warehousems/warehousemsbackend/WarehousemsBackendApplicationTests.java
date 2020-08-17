c4fb1e9c6b44b9350c468fc779de843cdbbc40ba
rungefuk <roman.ungefuk@gmail.com>
2020-08-18 01:12:12 +0300

    Создал сервер аутентификации. Создал для теста пару REST методов.

---
+++ warehousems-backend/src/test/java/ru/warehousems/warehousemsbackend/WarehousemsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.warehousems.warehousemsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehousemsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
