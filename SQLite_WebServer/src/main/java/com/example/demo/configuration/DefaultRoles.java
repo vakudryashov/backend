1da5eb6d9c6386d8191993f119186217193cc0e7
Vladimir Kudryashov <vakudryashov@inbox.ru>
2020-08-24 22:40:58 +0300

    Удалил старую копию приложения

--- SQLite_WebServer/src/main/java/com/example/demo/configuration/DefaultRoles.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.configuration;
-
-public enum DefaultRoles {
-    ROLE_ADMIN,
-    ROLE_USER
-}
--- SQLite_WebServer/src/main/java/com/example/demo/dao/UserDao.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.dao;
-
-import com.example.demo.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDao extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
