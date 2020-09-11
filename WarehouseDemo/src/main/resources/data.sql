4358ad08064ad045a226b6519c0402c9bda635b1
Vladimir Kudryashov <vakudryashov@inbox.ru>
2020-09-11 15:38:40 +0300

    Добавил заготовки для интерфейса управления привилегиями пользователей (файлы с ресурсами)

--- WarehouseDemo/src/main/resources/data.sql
+++ WarehouseDemo/src/main/resources/data.sql
@@ -1,2 +1,6 @@
 -- Insert data
-insert ignore into roles values (1,'ROLE_USER'),(2,'ROLE_ADMIN');
+insert ignore into roles
+    (id, symbol, name, active)
+values
+    (1,'ROLE_USER','Пользователь',true),
+    (2,'ROLE_ADMIN','Администратор',true);
